65899f34a6b1cdd988194e779c241de0cc7748e8
KlentDev <klentmicko@gmail.com>
2025-05-30 20:12:48 +0800

    updated header

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -3,12 +3,12 @@ const OpenGraphHead = () => (
   <Head>
     <title>Klent Micko | Creative Technologist</title>
     <meta name="description" content="My personal solace place on web-earth." />
-    <meta property="og:title" content="Klent Micko | Creative Technologist" />
+    <meta property="og:title" content="Klent Micko | Creative Developer" />
     <meta property="og:site_name" content="Klent Micko Bering" />
     <meta property="og:url" content="https://" />
     <meta
       property="og:description"
-      content="Ohh you found me?. Howdy! I am Klent Micko. Or you could call me Micko. That works too . . . I am a Creative Technologist."
+      content="Ohh you found me?. Howdy! I am Klent Micko. Or you could call me Micko. That works too . . . I am a Creative Developer."
     />
     <meta property="og:type" content="profile" />
     <meta property="og:image" content="https://"></meta>
